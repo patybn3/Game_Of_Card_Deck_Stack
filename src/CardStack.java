c8eecc5af5ea7f9d064da9bacef04d1ab3a322ff
patybn3 <paty.nigro@gmail.com>
2022-10-16 17:41:21 -0400

    CardStack.java, implemented methods.

--- src/CardStack.java
+++ src/CardStack.java
@@ -1,2 +1,44 @@
-public class CardStack {
+import java.util.Collections;
+import java.util.*;
+
+public class CardStack implements Project1StackInterface<Card>
+{
+    private Stack<Card> cardsStack;
+    protected int countOne = 0;
+    protected int countTwo = 0;
+
+    @Override
+    public void push(int suit, int face) {
+        
+    }
+
+    @Override
+    public Card pop() {
+        return null;
+    }
+
+    @Override
+    public Card top() {
+        return null;
+    }
+
+    @Override
+    public Card popTop() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
 }
--- src/CardStack_PatriciaAntlitz.java
+++ src/CardStack_PatriciaAntlitz.java
@@ -2,6 +2,7 @@ public class CardStack_PatriciaAntlitz
 {
     public static void main(String[] args)
     {
-
+        CardStack newStack = new CardStack();
+        newStack.playCards();
     }
 }
