098c6eea7e78580f903173c5243f671325355722
patybn3 <paty.nigro@gmail.com>
2022-10-16 17:48:29 -0400

    playCards() - game logic

--- src/CardStack.java
+++ src/CardStack.java
@@ -89,4 +89,84 @@ public class CardStack implements Project1StackInterface<Card>
     {
         Collections.shuffle(cardsStack);
     }
+
+    public void playCards()
+    {
+        //create two players
+        Card player1Card, player2Card;
+        int i = 0;
+
+        System.out.println("There are "+ size() + " cards in the deck");
+        //6 rounds, even number to allow ties
+        while(i < 6)
+        {
+            System.out.println("Deck is being shuffled\n");
+            shuffle();
+            //player 1 gets a card
+            player1Card = pop();
+            System.out.print("Player 1 drew the " + player1Card);
+            System.out.println(" - " + size() + " cards left in the deck");
+            //player 2 gets a card
+            player2Card =  pop();
+            System.out.print("Player 2 drew the " + player2Card);
+            System.out.println(" - " + size() + " cards left in the deck\n");
+
+            // order suits = {"CLUBS", "DIAMONDS", "HEARTS", "SPADES"};
+            //comparison
+            if(player1Card.getSuit() == player2Card.getSuit()) //when the suits of each player are equal, ex. HEARTS $ HEARTS
+            {
+                //if the face (number) is also equal
+                if(player1Card.getFace() == player2Card.getFace())
+                {
+                    System.out.println("Its a tie!! \n- Player 1 with " + player1Card);
+                    System.out.println("- Player 2 with " + player2Card + "\n");
+                    //both players get points
+                    countOne++;
+                    countTwo++;
+                }
+                else if(player1Card.getFace() > player2Card.getFace()) //if the value of player 1 is greater, Player 1 is the winner
+                {
+                    System.out.println("Round Winner = PLayer 1 with " + player1Card + "\n");
+                    countOne++;
+                }
+                else
+                {   //if player 2 is greater, player 2 is the winner
+                    System.out.println("Round Winner = Player 2 with " + player2Card + "\n");
+                    countTwo++;
+                }
+
+            }
+            else if (player1Card.getSuit() > player2Card.getSuit()) //when the value of player 1 suit is greater than player 2
+            {
+                //player 1 is the winner
+                System.out.println("Round Winner = PLayer 1 with " + player1Card + "\n");
+                countOne++;
+            }
+            else
+            {
+                //if not greater (player 2 is greater), then player two is the winner
+                System.out.println("Round Winner = Player 2 with " + player2Card +  "\n");
+                countTwo++;
+            }
+            //loops 6 times
+            i++;
+        }
+
+        //counting points
+        if(countOne == countTwo) //points are equal, its a tir
+        {
+            System.out.println("Player 1 points: " + countOne + " vs. Player 2 points: " + countTwo);
+            System.out.println("\nWe have a tie!!!");
+        }
+        else if(countOne > countTwo) //player 1 is the winner
+        {
+            System.out.println("Player 1 points: " + countOne + " vs. Player 2 points: " + countTwo);
+            System.out.println("\nWINNER = Player 1");
+        }
+        else //player 2 is the winner
+        {
+            System.out.println("Player 1 points: " + countOne + " vs. Player 2 points: " + countTwo);
+            System.out.println("\nWINNER = Player 2");
+        }
+    }
 }
