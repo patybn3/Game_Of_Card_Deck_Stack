6e0ab7611268097b5601e5a9ca62d5465754e6e4
patybn3 <paty.nigro@gmail.com>
2022-10-16 17:38:12 -0400

    Card.java - variables

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,12 @@
-public class Card {
+import java.util.Stack;
+
+public class Card<T> extends Stack<Card>
+{
+    // I read this source https://fpl.cs.depaul.edu/jriely/thinkapjava/thinkapjava.array2.html
+    public static final String[] SUITS = {"CLUBS", "DIAMONDS", "HEARTS", "SPADES"};
+    public static final String[] FACES = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "ACE"};
+
+    private int suit;
+    private int face;
+
 }
