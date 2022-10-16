6b508f78218b45b76f417acd523f55814a166b2e
patybn3 <paty.nigro@gmail.com>
2022-10-16 17:39:47 -0400

    interface

--- src/Project1StackInterface.java
+++ src/Project1StackInterface.java
@@ -1,2 +1,16 @@
-public interface Project1StackInterface {
+public interface Project1StackInterface<T>
+{
+    void push(int suit, int face); //adds and element to the stack
+
+    T pop(); //removes the top element of the stack, no return
+
+    T top(); //returns the top element of the stack
+
+    T popTop(); //does a top then a pop
+
+    int size(); //returns the number of elements in the stack
+
+    void clear(); //empties the stack
+
+    boolean isEmpty(); //returns true if the stack is empty
 }
